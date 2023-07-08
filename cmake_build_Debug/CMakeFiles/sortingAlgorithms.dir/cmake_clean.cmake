ec716106c8a72609049e6de596f928aea9f8ab60
HarryR <98973159+WindowBender@users.noreply.github.com>
2023-07-08 18:25:55 +0100

    began project

---
+++ cmake_build_Debug/CMakeFiles/sortingAlgorithms.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortingAlgorithms.dir/src/Game.cpp.o"
+  "CMakeFiles/sortingAlgorithms.dir/src/Game.cpp.o.d"
+  "CMakeFiles/sortingAlgorithms.dir/src/main.cpp.o"
+  "CMakeFiles/sortingAlgorithms.dir/src/main.cpp.o.d"
+  "sortingAlgorithms"
+  "sortingAlgorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sortingAlgorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake_build_Debug/CMakeFiles/sortingAlgorithms.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sortingAlgorithms.
+# This may be replaced when dependencies are built.
